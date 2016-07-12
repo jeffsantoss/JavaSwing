a8daf5b5d870c5fae69b8de3cccebb8632ed6e92
Jefferson Santos <jefferson.npng1@gmail.com>
2016-07-12 12:42:33 -0300

    Add files via upload
    Nesse exemplo é mostrado como será usado a classe FocusListener dentro de um JTextField, com ele você pode alterar o foco do texto,.Ao clicar dentro do campo o método focusGained é chamado e ao perder o foco chama-se o método focusLost, os dois métodos são sobreescritos da classe FocusListener.  O exemplo mostra como funciona.

---
+++ FocusJTextField.java
@@ -0,0 +1,49 @@
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.*;
+
+public class FocusJTextField extends JFrame {
+
+JTextField field1;
+JTextField field2;
+JPanel panel;
+
+ public FocusJTextField() {
+	 
+  super( "This is my Frame" );
+  panel = new JPanel();
+  field1 = new JTextField( 10 );
+  field2 = new JTextField( 10 );
+  field1.addFocusListener(new FocusListener(){
+
+	@Override
+	public void focusGained(FocusEvent arg0) {
+		field1.setText("");	
+	}
+
+	@Override
+	public void focusLost(FocusEvent arg0) {
+		field1.setText("Please put any text.");	
+	}
+	  
+  });
+  panel.add( new JLabel("Field 1:"));
+  panel.add( field1 );
+  panel.add( new JLabel("Field 2:"));
+  panel.add( field2 );
+  getContentPane().add( "Center", panel );
+  addWindowListener( new WindowAdapter() {
+   public void windowOpened( WindowEvent e ){
+        field1.requestFocus();
+     }
+   } ); 
+  pack();
+  setVisible( true );
+  }
+ 
+ 	public static void main(String[] args) {
+	 	new FocusJTextField();
+ }
+}
+
